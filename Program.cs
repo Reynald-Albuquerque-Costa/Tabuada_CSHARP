8085d8349ce97fc0176efd6e0271d6787ae7a04d
Reynald Albuquerque <reynaldalbuquerquecosta@gmail.com>
2022-07-18 10:39:07 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tabuada_CSHARP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("ENTRADA: ");
+            double entrada = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("SAIDA: ");
+           
+            for (int x = 0; x <= 10; x++)
+            {
+                double total = entrada * x;
+                Console.WriteLine(entrada + " X " + x + " = " + total);
+            }
+
+        }
+    }
+}
